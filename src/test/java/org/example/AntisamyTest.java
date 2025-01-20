9e355bba0b720f175e4cc86fdd864cad6b7f02ae
Jonah Klöckner <jonah.kloeckner@1und1.de>
2025-01-20 11:59:19 +0100

    adds MRE

---
+++ src/test/java/org/example/AntisamyTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.owasp.validator.html.AntiSamy;
+import org.owasp.validator.html.CleanResults;
+import org.owasp.validator.html.Policy;
+import org.owasp.validator.html.PolicyException;
+import org.owasp.validator.html.ScanException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class AntisamyTest {
+
+    @Test
+    void testAntisamy() throws PolicyException, ScanException {
+        //Given
+        AntiSamy antiSamy = new AntiSamy();
+        String taintedHtml = "<html><body><style>.cl { color: rgb(0%,0%,0%); }</style></body></html>";
+        Policy policy = Policy.getInstance();
+
+        System.out.println(policy);
+
+        //When
+        CleanResults results = antiSamy.scan(taintedHtml, policy);
+
+        //Then
+        assertEquals(0, results.getNumberOfErrors());
+        assertNotNull(results.getCleanHTML());
+    }
+}
